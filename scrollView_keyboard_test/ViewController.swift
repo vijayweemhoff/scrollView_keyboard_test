0b88feced37e22e8c780d528a1cd0f48bc873dbb
Vijay Weemhoff <vijay.w@hotmail.com>
2016-06-02 14:20:45 +0200

    Initial Commit

---
+++ scrollView_keyboard_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrollView_keyboard_test
+//
+//  Created by Vijay Weemhoff on 02-06-16.
+//  Copyright © 2016 Vijay Weemhoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ scrollView_keyboard_testTests/scrollView_keyboard_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  scrollView_keyboard_testTests.swift
+//  scrollView_keyboard_testTests
+//
+//  Created by Vijay Weemhoff on 02-06-16.
+//  Copyright © 2016 Vijay Weemhoff. All rights reserved.
+//
+
+import XCTest
+@testable import scrollView_keyboard_test
+
+class scrollView_keyboard_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
